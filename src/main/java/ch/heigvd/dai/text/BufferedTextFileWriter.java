7f33b4819dc69afb7cced686447da451f9806952
Cobora2001 <thomasvu@bluewin.ch>
2023-10-03 17:55:54 +0200

    Add reader/writer

--- src/main/java/ch/heigvd/dai/text/BufferedTextFileWriter.java
+++ src/main/java/ch/heigvd/dai/text/BufferedTextFileWriter.java
@@ -2,14 +2,16 @@ package ch.heigvd.dai.text;
 
 import ch.heigvd.dai.Writable;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class BufferedTextFileWriter implements Writable {
     @Override
     public void write(String filename, int sizeInBytes) throws IOException {
-        // TODO : implement this method
-        throw new UnsupportedOperationException("Not implemented yet");
+        BufferedWriter bos = new BufferedWriter(new FileWriter(filename));
+        for(int i = 0; i < sizeInBytes; ++i) {
+            bos.write('a');
+        }
+        bos.flush();
+        bos.close();
     }
 }
